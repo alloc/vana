ea0caca124cf3352ed93ca2bfaaf4217bf3f5436
Alec Larson <alec.stanford.larson@gmail.com>
2019-03-09 19:32:48 -0500

    fix: export IObserver

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@ export { o } from './funcs/o'
 export { tap, Tapped } from './funcs/tap'
 export { watch, WatchProxy } from './funcs/watch'
 export { revise } from './funcs/revise'
-export { Observable, isObservable, getObservable } from './types/Observable'
+export { Observable, IObserver, isObservable, getObservable } from './types/Observable'
 
 // Immer exports
 export { Draft, Immutable, isDraft, nothing, original, immerable } from 'immer'
