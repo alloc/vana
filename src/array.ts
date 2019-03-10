d7ced3eb326cfe18b25e660200acc26e1937c63d
Alec Larson <alec.stanford.larson@gmail.com>
2019-03-09 19:41:12 -0500

    feat: add vana/lib/array

---
+++ src/array.ts
@@ -0,0 +1,67 @@
+import { become } from './funcs/become'
+
+type AnyArray = ReadonlyArray<any>
+
+export function append<T>(
+  base: ReadonlyArray<T>,
+  first: T,
+  ...rest: T[]
+): typeof base
+
+export function append(base: AnyArray, ...values: any[]) {
+  const copy = base.slice()
+  copy.push(...values)
+  return become(base, copy)
+}
+
+export function prepend<T>(
+  base: ReadonlyArray<T>,
+  first: T,
+  ...rest: T[]
+): typeof base
+
+export function prepend(base: AnyArray, ...values: any[]) {
+  return become(base, values.concat(base))
+}
+
+export function insert<T>(
+  base: ReadonlyArray<T>,
+  index: number,
+  first: T,
+  ...rest: T[]
+): typeof base
+
+export function insert(base: AnyArray, index: number, ...values: any[]) {
+  return become(base, base.slice(0, index).concat(values, base.slice(index)))
+}
+
+export function concat<T>(
+  base: ReadonlyArray<T>,
+  first: Exclude<T, AnyArray> | ReadonlyArray<T>,
+  ...rest: Array<typeof first>
+): typeof base
+
+export function concat(base: AnyArray, ...values: any[]) {
+  const copy = base.concat(...values)
+  return copy.length == base.length ? base : become(base, copy)
+}
+
+export function remove<T>(
+  base: ReadonlyArray<T>,
+  index: number,
+  count: number = 1
+): typeof base {
+  if (count > 0 && index < base.length && index + count > 0) {
+    const copy =
+      index <= 0
+        ? base.slice(index + count)
+        : index + count >= base.length
+        ? base.slice(0, index)
+        : base.slice(0, index).concat(base.slice(index + count))
+
+    if (copy.length !== base.length) {
+      return become(base, copy)
+    }
+  }
+  return base
+}
