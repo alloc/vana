008ca456257909caa373f03a9747de2f5fd1fc0b
Alec Larson <alec.stanford.larson@gmail.com>
2019-03-18 09:26:02 -0400

    feat: add "keepAlive" function

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,13 @@ export { o } from './funcs/o'
 export { tap, Tapped } from './funcs/tap'
 export { watch, WatchProxy } from './funcs/watch'
 export { revise } from './funcs/revise'
-export { Observable, IObserver, isObservable, getObservable } from './types/Observable'
+export { keepAlive } from './funcs/keepAlive'
+export {
+  Observable,
+  IObserver,
+  isObservable,
+  getObservable,
+} from './types/Observable'
 
 // Immer exports
 export { Draft, Immutable, isDraft, nothing, original, immerable } from 'immer'
