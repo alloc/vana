04260bbb5d74ad080f346afe096587031d1a9e6d
Alec Larson <alec.stanford.larson@gmail.com>
2019-03-19 12:51:30 -0400

    test: improve OProp tests

--- spec/OProps.spec.ts
+++ spec/OProps.spec.ts
@@ -1,5 +1,5 @@
 import { o, revise, tap, watch } from '../src'
-import { getObservable } from '../src/types/Observable'
+import { getObservable, getObservers } from '../src/types/Observable'
 import { OProp, OProps } from '../src/types/OProps'
 
 describe('new OProps()', () => {
@@ -13,11 +13,78 @@ describe('new OProps()', () => {
   })
 
   describe('when a property value is observable', () => {
-    it('creates an OProp<T> for the property name', () => {
+    it('creates an OProp for the property name', () => {
       let base = o({ a: {} })
       let { watched } = getObservable(base) as OProps
-      expect(watched).toBeDefined()
       expect(watched!.a).toBeInstanceOf(OProp)
+    })
+
+    it('keeps its OProp activated when its last observer is removed', () => {
+      let base = o({ a: {} })
+      let { watched } = getObservable(base) as OProps
+      let observer = watch(base, 'a').tap(() => {})
+      observer.dispose()
+      expect(watched!.a).toBeInstanceOf(OProp)
+    })
+  })
+
+  describe('when a property value becomes observable', () => {
+    it('deeply observes its value', () => {
+      let base: any = o({ a: 1 })
+      let baseObservable = getObservable(base) as OProps
+      expect(baseObservable.watched).toBeUndefined()
+      baseObservable.tap(copy => {
+        base = copy
+      })
+
+      // Set the property to an observable object.
+      revise(base, { a: { b: { c: 1 } } })
+      expect(baseObservable.watched!.a).toBeInstanceOf(OProp)
+
+      // Set a deep property.
+      let prevBase = base
+      revise(base.a.b, { c: 2 })
+
+      // Ensure `base` was updated.
+      expect(base).not.toBe(prevBase)
+      expect(base.a.b.c).toBe(2)
+    })
+  })
+
+  describe('when a property goes from being observable to not', () => {
+    it('deactivates its OProp when no observers exist', () => {
+      let base: any = o({ a: {} })
+      let baseObservable = getObservable(base) as OProps
+
+      revise(base, { a: undefined })
+      expect(baseObservable.watched!.a).toBeUndefined()
+    })
+
+    it('keeps its OProp activated when observers exist', () => {
+      type Base = { a?: { b: number } }
+      let base: Base = o({ a: { b: 1 } })
+      let baseObservable = getObservable(base) as OProps
+      let propObservable = baseObservable.watched!.a!
+      let observer = watch(base, 'a').tap(() => {})
+
+      revise(base, { a: undefined })
+      expect(baseObservable.watched!.a).toBeDefined()
+      expect(getObservers(propObservable)).toEqual([observer])
+    })
+  })
+
+  describe('order of observers for a nested observable', () => {
+    it('ensures the first observer is its OProp', () => {
+      let nested = o({ b: 1 })
+      let observer = tap(nested, () => {})
+      expect(getObservers(nested)).toEqual([observer])
+
+      let base: any = o({ a: nested })
+      let baseObservable = getObservable(base) as OProps
+      expect(getObservers(nested)).toEqual([
+        baseObservable.watched!.a,
+        observer,
+      ])
     })
   })
 
@@ -55,20 +122,6 @@ describe('new OProps()', () => {
         revise(base.a, { b: 1 })
         expect.assertions(1)
       })
-    })
-  })
-
-  describe('when a property goes from being observable to not', () => {
-    it('preserves the property if it still has observers', () => {
-      type Base = { a?: { b: number } }
-      let base: Base = o({ a: { b: 1 } })
-      let baseObservable = getObservable(base) as OProps
-      let propObservable = baseObservable.watched!.a!
-      let observer = watch(base, 'a').tap(() => {})
-
-      revise(base, { a: undefined })
-      expect(baseObservable.watched).toBeDefined()
-      expect(propObservable._observers).toEqual([observer])
     })
   })
 })
