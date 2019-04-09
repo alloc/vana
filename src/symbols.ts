e9bde238368c14f0b20e9aae3c30df3a5e3823c2
Alec Larson <alec.stanford.larson@gmail.com>
2019-04-09 18:45:01 -0400

    0.1.5

--- src/symbols.ts
+++
@@ -1,5 +0,0 @@
-/** Unique symbol for the internal Observable instance */
-export const $O = Symbol.for('vana:observable')
-
-/** Unique symbol for debug mode */
-export const $DEBUG = Symbol.for('vana:debug')
