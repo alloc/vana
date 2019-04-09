16839405ee164156056d087366a38639006360f2
Alec Larson <alec.stanford.larson@gmail.com>
2019-04-09 16:13:06 -0400

    0.1.4

---
+++ lib/types/OBinding.d.ts
@@ -0,0 +1,17 @@
+import { IDisposable } from '../common';
+import { Observable } from './Observable';
+export declare type Driver<T = any> = (next: (value: T) => void) => IDisposable | (() => void) | void;
+/**
+ * An observable binding to some other stream-like mechanism.
+ *
+ * The binding is lazy, which means no value exists until observed.
+ *
+ * The current value is always `undefined` when no observers exist.
+ */
+export declare class OBinding<T = any> extends Observable<T> {
+    driver: Driver<T>;
+    private _binding?;
+    constructor(driver: Driver<T>);
+    protected activate(): void;
+    protected deactivate(): void;
+}
---
+++ lib/types/index.d.ts
@@ -0,0 +1,11 @@
+import { Tapped } from '../funcs/tap';
+import { Observable } from './Observable';
+/** Useful for `subscribe` functions built on `vana` */
+export declare type Observed<T> = T extends Observable<infer U> ? U : Tapped<T>;
+export { Observable, IObserver } from './Observable';
+export { OBinding } from './OBinding';
+export { OPath } from './OPath';
+export { OPromise } from './OPromise';
+export { OProps } from './OProps';
+export { OTuple, ObservedTuple } from './OTuple';
+export { Change, IChangeTarget } from './Change';
