11e51583717269ff0f794c8a6ad0d16460323ffb
Alec Larson <alec.stanford.larson@gmail.com>
2019-04-30 18:21:32 -0400

    0.1.6

---
+++ lib/index.d.ts
@@ -0,0 +1,11 @@
+export { o } from './funcs/o';
+export { tap, Tapped } from './funcs/tap';
+export { watch, WatchProxy } from './funcs/watch';
+export { revise } from './funcs/revise';
+export { latest } from './funcs/latest';
+export { keepAlive } from './funcs/keepAlive';
+export { Observable, IObserver, isObservable, getObservable, } from './types/Observable';
+export { Draft, Immutable, isDraft, nothing, original, immerable } from 'immer';
+export { Recipe, produce, setUseProxies, setAutoFreeze } from './immer';
+export { freeze, Frozen } from './funcs/freeze';
+export { Disposable, IDisposable, IThenable } from './common';
