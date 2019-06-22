d90b43acd4369ad28ab131094016fd6faa7da021
Alec Larson <alec.stanford.larson@gmail.com>
2019-06-22 16:00:59 -0400

    0.5.5

--- src/shared/symbols.ts
+++
@@ -1,8 +0,0 @@
-/** Unique symbol for the internal Observable instance */
-export const $O = Symbol.for('vana:observable')
-
-/** Unique symbol for observables that always have the latest value */
-export const $ALIVE = Symbol.for('vana:alive')
-
-/** Unique symbol for debug mode */
-export const $DEBUG = Symbol.for('vana:debug')
